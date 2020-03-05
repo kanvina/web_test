ed430da1cb389c2413f03180f25a80c30b795322
wangyandi <1069704056@qq.com>
2020-03-05 20:34:44 +0800

    2020 03 05

--- js/js_01.js
+++ js/js_01.js
@@ -1,9 +1,9 @@
-var streets=L.tileLayer('http://mt1.google.cn/vt/lyrs=m@207000000&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=Galile')
-var satellite= L.layerGroup([
+var streets = L.tileLayer('http://mt1.google.cn/vt/lyrs=m@207000000&hl=zh-CN&gl=CN&src=app&x={x}&y={y}&z={z}&s=Galile')
+var satellite = L.layerGroup([
     L.tileLayer('http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gali'),
     L.tileLayer('http://mt1.google.cn/vt/imgtp=png32&lyrs=h@207000000&hl=zh-CN&gl=cn&x={x}&y={y}&z={z}&s=Galil')
 ]);
-var satellite_nomarker =L.tileLayer('http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gali')
+var satellite_nomarker = L.tileLayer('http://mt1.google.cn/vt/lyrs=s&hl=zh-CN&gl=CN&x={x}&y={y}&z={z}&s=Gali')
 var baseLayers = {
     "街道图": streets,
     "影像图": satellite,
@@ -12,58 +12,58 @@ var baseLayers = {
 var map = L.map('map', {
     center: [39.9, 116.4],
     zoom: 6,
-    layers:[streets]
+    layers: [streets]
 });
+L.control.layers(baseLayers).addTo(map);
 
 
-function print_json(name) {
-
-    $.getJSON("data/data.json", function (data) {
-
-        $('#input_text').val(data.rows[0][name])
-    })
-
-};
-
-function btn_click() {
-    key = $('#select_id option:selected').val();
-    print_json(key)
-}
-
-function onload_show() {
-    // 创建地图实例
-
-    // 添加图层到地图
-
-    L.control.layers(baseLayers).addTo(map);
-    
-}
-
-function show_loction(){
 
 
-    $.getJSON("../data/beijing_json.json", function (data) {
-      
-        var points_list=data.features[0].geometry.coordinates[0][0]
+// function show_loction(){
 
-        var location_new_list=[]
+//     $.getJSON("../data/beijing_json.json", function (data) {
 
-        for (var i =0;i<points_list.length;i++){
-            location_xy=points_list[i]
-            x=location_xy[0]
-            y=location_xy[1]
-            var location_new=[]
-            location_new.push(y)
-            location_new.push(x)
-            location_new_list.push(location_new)
+//         var points_list=data.features[0].geometry.coordinates[0][0]
+
+//         var location_new_list=[]
+
+//         for (var i =0;i<points_list.length;i++){
+//             location_xy=points_list[i]
+//             x=location_xy[0]
+//             y=location_xy[1]
+//             var location_new=[]
+//             location_new.push(y)
+//             location_new.push(x)
+//             location_new_list.push(location_new)
+//         };
+
+//       //根据提供的三点坐标绘制折线
+//      var polyline = L.polyline(location_new_list, { color: 'red' });
+//      polyline.addTo(map);
+//      var center_zoom=polyline.getBounds()
+//      map.fitBounds(center_zoom);//缩放至屏幕
+//     })
+
+// }
+
+function show_loction() {
+    $.getJSON("../data/suqian_geojson.geojson", function (data) {
+
+        var myStyle={
+            "color": "red",
+            "weight": 1,
+            "opacity": 0.8
         };
+        var geo_border = L.geoJSON(data, {style:myStyle});
         
 
-      //根据提供的三点坐标绘制折线
-     var polyline = L.polyline(location_new_list, { color: 'red' });
-     polyline.addTo(map);
+        // geo_border.eachLayer(function (layer) {
+        //     layer.bindPopup(layer.features.properties.NAMEC);
+        // });
         
-
+       
+        geo_border.addTo(map);
+        map.fitBounds(geo_border.getBounds());
 
     })
 
